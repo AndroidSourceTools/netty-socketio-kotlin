9a2310bf0db7cbc7add98efce7235cd668590505
Gray Cat Developer <gray.cat.developer@gmail.com>
2020-07-23 10:58:42 +0300

    Setup

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "1.4-M3"
+}
+
+group = "com.corundumstudio.socketio"
+version = "1.7.19-SNAPSHOT"
+
+repositories {
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
