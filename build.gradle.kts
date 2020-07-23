f7c32c455b5fac46f0b057044ab47a10bf5c6cb5
Gray Cat Developer <gray.cat.developer@gmail.com>
2020-07-23 11:20:18 +0300

    Setup: ctrl+c -> ctrl+v

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,33 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+
+    val nettyVersion = "4.1.50.Final"
+    implementation("io.netty:netty-buffer:$nettyVersion")
+    implementation("io.netty:netty-common:$nettyVersion")
+    implementation("io.netty:netty-transport:$nettyVersion")
+    implementation("io.netty:netty-handler:$nettyVersion")
+    implementation("io.netty:netty-codec-http:$nettyVersion")
+    implementation("io.netty:netty-codec:$nettyVersion")
+    implementation("io.netty:netty-transport-native-epoll:$nettyVersion")
+
+    implementation("org.slf4j:slf4j-api:1.7.21")
+
+    val jacksonVersion = "2.10.4"
+    implementation("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
+    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
+
+    val springVersion = "3.1.4.RELEASE"
+    compileOnly("org.springframework:spring-beans:$springVersion")
+    compileOnly("org.springframework:spring-core:$springVersion") {
+        exclude(group = "commons-logging", module = "commons-logging")
+    }
+
+    compileOnly("org.redisson:redisson:3.13.1")
+    compileOnly("com.hazelcast:hazelcast-client:3.12.7")
+
+    testImplementation("junit:junit:4.11")
+    testImplementation("org.jmockit:jmockit:1.39")
 }
 
 tasks {
